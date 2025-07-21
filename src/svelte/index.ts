168b1da412e089f2fc3872defeb173e5b1301dd7
Siddharth <siddharth.060605@gmail.com>
2025-07-21 17:09:02 -0400

    Begin adding packaging

---
+++ src/svelte/index.ts
@@ -0,0 +1,7 @@
+import ZxForm from '@/svelte/ZxForm.svelte';
+import ZxInput from '@/svelte/ZxInput.svelte';
+
+export {
+    ZxForm,
+    ZxInput
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,26 @@
-import tailwindcss from '@tailwindcss/vite';
-import { defineConfig } from 'vite';
 import path from 'path';
+import { defineConfig } from 'vite';
+
 import { svelte } from '@sveltejs/vite-plugin-svelte';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
   // root: "./preview",
   plugins: [svelte(), tailwindcss()],
   resolve: {
-    alias: { '@': path.resolve(__dirname, 'src') }
-  }
+    alias: { '@': path.resolve('src') }
+  },
+  
+  build: {
+    lib: {
+      name: 'ZxForms',
+      entry: {
+        svelte: path.resolve('src/svelte/index.ts'),
+      }
+    },
+    rollupOptions: {
+      external: ['svelte', 'react'], // optional
+    },
+  },
 });
